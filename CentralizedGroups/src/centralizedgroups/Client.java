c29da99f749f1c71edd74f33719678d351d93e36
greenlucid <greenfirechaos@gmail.com>
2019-11-05 12:59:07 +0100

    Added even options for Client

--- CentralizedGroups/src/centralizedgroups/Client.java
+++ CentralizedGroups/src/centralizedgroups/Client.java
@@ -13,6 +13,7 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -81,11 +82,15 @@ public class Client extends UnicastRemoteObject implements ClientInterface {
                 System.out.println("Opcion no valida: Numero no recogido en las opciones");
                 continue;
             } else {
+                String groupAlias;
+                String ownerAlias;
+                String alias;
                 switch(option){
+                    
                     case (1):
                         
                         System.out.println("Name an alias for the group: ");
-                        String groupAlias = s.nextLine();
+                        groupAlias = s.nextLine();
                         int res = server.createGroup(groupAlias, client.clientAlias, client.hostname);
                         if(res == -1){
                             System.out.println("ERROR al crear el grupo");
@@ -94,18 +99,45 @@ public class Client extends UnicastRemoteObject implements ClientInterface {
                         }
                         break;
                     case (2):
+                        System.out.println("Nombra el grupo que quieras eliminar");
+                        groupAlias = s.nextLine();
+                        System.out.println("Nombra el propietario del grupo");
+                        ownerAlias = s.nextLine();
+                        if(!server.removeGroup(groupAlias, ownerAlias))
+                            System.out.println("ERROR al eliminar grupo");
+                        else
+                            System.out.println("Grupo eliminado");
                         break;
                     case (3):
                         break;
                     case (4):
+                        System.out.println("Nombra el grupo del que quieras eliminar");
+                        groupAlias = s.nextLine();
+                        System.out.println("Nombra el usuario al que quieras expulsar. No puedes eliminar al owner");
+                        alias = s.nextLine();
+                        if(!server.removeMember(groupAlias, alias))
+                            System.out.println("ERROR: usuario no expulsado");
+                        else
+                            System.out.println("El usuario "+alias+" ha sido expulsado de "+groupAlias);
                         break;
                     case (5):
                         break;
                     case (6):
+                        System.out.println("Nombra el grupo a desbloquear");
+                        groupAlias = s.nextLine();
+                        if(!server.AllowMembers(groupAlias))
+                            System.out.println("ERROR: El grupo no existe");
+                        else
+                            System.out.println(groupAlias + " ha sido desbloqueado");
                         break;
                     case (7):
                         break;
                     case (8):
+                        System.out.println("Lista de grupos actuales del servidor:");
+                        LinkedList<String> namelist = server.ListGroup();
+                        for(int i = 0; i<namelist.size(); i++){
+                            System.out.println((i+1)+": "+namelist.get(i));
+                        }
                         break;
                     case (9):
                         System.exit(0);
